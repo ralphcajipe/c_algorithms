420d1ebb5f65c39ea033c3ad943dc10dac30ce9b
Ralph Cajipe <ralphcajipe@gmail.com>
2022-01-23 20:48:43 +0800

    Started project

---
+++ linear_search/numbers.c
@@ -0,0 +1,16 @@
+// Implements linear search for numbers
+#include <stdio.h>
+
+int main(void) {
+    int numbers[] = {4, 6, 8, 2, 5, 0};
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == -1) {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
